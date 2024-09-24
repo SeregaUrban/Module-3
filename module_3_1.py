c026c63b920007d3f853606f60434b2952712d62
SeregaUrban <7236187@mail.ru>
2024-09-25 00:41:04 +0300

    module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -4,7 +4,7 @@ def count_calls():
     calls = calls + 1
 def string_info(string):
     count_calls()
-    return {len(string) , string.upper() , string.lower() }
+    return [len(string) , string.upper() , string.lower() ]
 def is_contains(string , list_to_search):
     count_calls()
     for i in list_to_search:
