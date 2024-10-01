64a044d6d2ea45166da9216e6fef31b8dbe9a09f
SeregaUrban <7236187@mail.ru>
2024-10-01 10:26:55 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+from logging.config import stopListening
+
+adress = ('.com' , '.ru' , '.net')
+def send_email (message , recipient, *, sender='university.help@gmail.com' ):
+    adress = ('.com', '.ru', '.net')
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif '@' not in recipient or '@' not in sender:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif not any(i in recipient for i in adress) or not any( i in sender for i in adress ):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == 'university.help@gmail.com' :
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+
+
